6a8820158f309611202d61629b03414c36386f21
VicktzZ <VictorHugo220Br@gmail.com>
2022-04-28 17:34:01 +0000

    Functions Complete

--- main.js
+++ main.js
@@ -5,15 +5,16 @@ import start from "./lib/start.js";
 
 (async function main(){
 
-    // // "Tela de carregamento"
-    // console.clear()
-    // let spinner = createSpinner('Carregando...')
-    // spinner.start()
-    // await sleep(5000)
-    // spinner.stop()
+    // "Tela de carregamento"
+    console.clear()
+    let spinner = createSpinner('Carregando...')
+    
+    spinner.start()
+    await sleep(5000)
+    spinner.stop()
 
-    // // Introdução
-    // await start()
+    // Introdução
+    await start()
     
     // Inicio do jogo
     await game()
