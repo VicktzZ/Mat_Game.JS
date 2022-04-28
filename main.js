f033e65796eda45630583d00d2368a502c7499b2
VicktzZ <VictorHugo220Br@gmail.com>
2022-04-28 04:10:59 +0000

    Questions

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-import load from "./lib/load.js";
+import { createSpinner } from "nanospinner";
+import { sleep } from "./lib/functions/misc.js";
+import { game } from "./lib/game.js";
 import start from "./lib/start.js";
 
 (async function main(){
 
-    await load()
-    await start()
+    // // "Tela de carregamento"
+    // console.clear()
+    // let spinner = createSpinner('Carregando...')
+    // spinner.start()
+    // await sleep(5000)
+    // spinner.stop()
 
+    // // Introdução
+    // await start()
+    
+    // Inicio do jogo
+    await game()
 })()
